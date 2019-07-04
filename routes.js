d20ea401528f088fe09741f2e1eff1b2915cfe7e
Glitch (ict-skillslab04) <none>
2019-07-04 18:08:46 +0000

    Lab4Exercises

--- routes.js
+++ routes.js
@@ -5,9 +5,13 @@ const router = express.Router();
 
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
+const playlist = require('./controllers/playlist.js');
 
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
+router.get('/playlist/:id', playlist.index);
+router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
+router.get('/playlist/:id/deleteplaylist/:playlistid', playlist.deletePlayList);
 
 module.exports = router;
